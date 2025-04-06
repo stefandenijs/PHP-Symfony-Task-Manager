52f6fd5673357c7cd03a784b5d3c1c6c861462b5
Stefan de Nijs <stefangithub@hotmail.com>
2025-04-06 20:52:35 +0200

    Remove unused code

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -37,29 +37,4 @@ class TaskRepository extends ServiceEntityRepository
         $em->persist($task);
         $em->flush();
     }
-
-//        /**
-//         * @return Task[] Returns an array of Task objects
-//         */
-//        public function findByExampleField($value): array
-//        {
-//            return $this->createQueryBuilder('t')
-//                ->andWhere('t.exampleField = :val')
-//                ->setParameter('val', $value)
-//                ->orderBy('t.id', 'ASC')
-//                ->setMaxResults(10)
-//                ->getQuery()
-//                ->getResult()
-//            ;
-//        }
-//
-//        public function findOneBySomeField($value): ?Task
-//        {
-//            return $this->createQueryBuilder('t')
-//                ->andWhere('t.exampleField = :val')
-//                ->setParameter('val', $value)
-//                ->getQuery()
-//                ->getOneOrNullResult()
-//            ;
-//        }
 }
