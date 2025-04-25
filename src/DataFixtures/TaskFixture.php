539cdf96ab3a17282ff76dff6b584637f0e9581c
Stefan de Nijs <stefangithub@hotmail.com>
2025-04-25 14:39:53 +0200

    TaskController.php tests

--- src/DataFixtures/TaskFixture.php
+++ src/DataFixtures/TaskFixture.php
@@ -18,6 +18,14 @@ class TaskFixture extends Fixture implements DependentFixtureInterface
             $task->setDescription('Task description ' . $i);
             $task->setOwner($this->getReference(UserFixture::TEST_USER, User::class));
             $manager->persist($task);
+            for ($j = 1; $j <= 5; $j++) {
+                $subTask = new Task();
+                $subTask->setTitle('SubTask ' . $j);
+                $subTask->setDescription('SubTask description ' . $j);
+                $subTask->setOwner($this->getReference(UserFixture::TEST_USER, User::class));
+                $manager->persist($subTask);
+                $task->addSubTask($subTask);
+            }
         }
 
         for ($i = 11; $i <= 20; $i++) {
@@ -26,6 +34,14 @@ class TaskFixture extends Fixture implements DependentFixtureInterface
             $task->setDescription('Task description ' . $i);
             $task->setOwner($this->getReference(UserFixture::TEST_USER2, User::class));
             $manager->persist($task);
+            for ($j = 1; $j <= 5; $j++) {
+                $subTask = new Task();
+                $subTask->setTitle('SubTask ' . $j);
+                $subTask->setDescription('SubTask description ' . $j);
+                $subTask->setOwner($this->getReference(UserFixture::TEST_USER2, User::class));
+                $manager->persist($subTask);
+                $task->addSubTask($subTask);
+            }
         }
 
         $manager->flush();
